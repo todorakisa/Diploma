9d84daad8b34abd9331c5354202eea034db85815
todorakisa <todor.domitrov2000@gmail.com>
2019-01-06 22:54:09 +0200

    repairing some of the css and html

--- BoardFind/assets/js/app.js
+++ BoardFind/assets/js/app.js
@@ -9,5 +9,4 @@
 require('../css/app.css');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+require('../images/logo.png');
